baa429c6c505eadd07213fc0f94edad3afd62571
nikitha <nikithakambhampati123@gmail.com>
2024-11-27 22:41:29 +0530

    my-maven-project

---
+++ src/main/java/org/company/my_maven_project/App.java
@@ -0,0 +1,10 @@
+package org.company.my_maven_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
